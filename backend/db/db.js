1d2deb45adb5e525eaed38cbc808247371deb0de
Krishna Singh <krishnasingh781630@gmail.com>
2024-01-23 03:35:08 +0530

    db connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://admin:1234567890@cluster01.i8pgo02.mongodb.net/paytm"
-);
+const mongoConnection = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:1234567890@cluster01.i8pgo02.mongodb.net/paytm"
+    );
+    console.log("Mongo connection established");
+  } catch (error) {
+    console.log("Mongo connection failed " + error);
+  }
+};
+
+mongoConnection();
 
 const userSchema = new mongoose.Schema({
   username: string,
+  firstName: string,
+  lastName: string,
   password: string,
 });
 
