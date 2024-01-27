8f59c7d1ed8e14f10464d20ec65efc4d60a1dc86
Krishna Singh <krishnasingh781630@gmail.com>
2024-01-28 01:33:27 +0530

    sign up page done

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignUp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="m-5 p-5 mx-auto mt-20 w-[350px] h-[580px] shadow-xl bg-slate-50 rounded-xl">
       <div className="text-center text-4xl font-bold mb-5">Sign Up</div>
@@ -10,6 +16,7 @@ const SignUp = () => {
       <div>
         <b>First Name </b> <br />
         <input
+          onChange={(e) => setFirstName(e.target.value)}
           className="bg-slate-50 my-3 p-2 rounded-lg border w-full"
           type="text"
           placeholder="John"
@@ -18,6 +25,7 @@ const SignUp = () => {
       <div>
         <b>Last Name </b> <br />
         <input
+          onChange={(e) => setLastName(e.target.value)}
           className="bg-slate-50 my-3 p-2 rounded-lg border w-full"
           type="text"
           placeholder="Doe"
@@ -26,6 +34,7 @@ const SignUp = () => {
       <div>
         <b>Email</b> <br />
         <input
+          onChange={(e) => setUsername(e.target.value)}
           className="bg-slate-50 my-3 p-2 rounded-lg border w-full"
           type="email"
           placeholder="johndoe@example.com"
@@ -34,14 +43,27 @@ const SignUp = () => {
       <div>
         <b>Password</b> <br />
         <input
+          onChange={(e) => setPassword(e.target.value)}
           className="bg-slate-50 my-3 p-2 rounded-lg border w-full"
           type="password"
         />
       </div>
-      <button className="mb-2 w-full px-10 py-2 bg-black text-white rounded-lg">
+      <button
+        onClick={() => {
+          axios.post("http://localhost:5000/api/v1/user/signup", {
+            firstName,
+            lastName,
+            username,
+            password,
+          });
+        }}
+        className="mb-2 w-full px-10 py-2 bg-black text-white rounded-lg"
+      >
         Sign Up
       </button>
-      <p className="font-medium text-center">Already have an account? <u>Login</u></p>
+      <p className="font-medium text-center">
+        Already have an account? <u>Login</u>
+      </p>
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import About from "./components/About";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp.jsx";
+import SignIn from "./components/SignIn.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
