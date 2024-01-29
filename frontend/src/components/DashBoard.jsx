9a453239daf9b4a8b8d245b155af80377fce3f11
Krishna Singh <krishnasingh781630@gmail.com>
2024-01-29 22:13:41 +0530

    dashboard almost done

--- frontend/src/components/DashBoard.jsx
+++ frontend/src/components/DashBoard.jsx
@@ -5,23 +5,37 @@ import axios from "axios";
 const DashBoard = () => {
   const [users, setUsers] = useState([]);
   const [filter, setFilter] = useState("");
+  const [balance, setBalance] = useState([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/v1/user/bulk?filter=" + filter)
       .then((response) => {
-        console.log(response.data.user);
+        // console.log(response.data);
         setUsers(response.data.user);
       });
   }, [filter]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/v1/account/balance", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setBalance(response.data.balance);
+        console.log(response.data.balance);
+      });
+  }, []);
   return (
     <div>
       <div className="w-full flex justify-between bg-slate-200 p-4 mt-5">
         <b>PayTM App</b>
-        <b>Hello Krishna</b>
+        <b>Hello {users[users.length - 1].firstName}</b>
       </div>
 
-      <div className="my-3 p-2 font-medium">Your balance Rs 10,000</div>
+      <div className="my-3 p-2 font-medium">Your balance Rs {Math.floor(balance)}</div>
       <div className="mt-3 p-2 font-medium">Users</div>
       <input
         onChange={(e) => setFilter(e.target.value)}
--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -7,9 +7,9 @@ const User = ({ user }) => {
         <span className="ml-2 bg-green-300 rounded-full px-3 py-2 mb-1">
           {user.firstName[0].toUpperCase()}
         </span>
-        <span className="ml-2 font-medium p-2">{user.firstName}</span>
+        <span className="ml-2 font-medium p-2">{user.firstName} {user.lastName}</span>
       </div>
-      <div className="p-2 mb-1 bg-gray-400 rounded-lg m-2">
+      <div className="p-2 mb-1 bg-gray-400 rounded-lg m-2 hover:bg-gray-500">
         Send Money
       </div>
     </div>
